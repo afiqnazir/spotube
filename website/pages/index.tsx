ce98d7c788deb891b0c2fd84e529a6724c456768
Afiq Nazir <62737721+afiqnazir@users.noreply.github.com>
2024-03-03 22:38:55 +0530

    Update index.tsx

--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -70,15 +70,15 @@ const Root = () => {
           >
             <chakra.div maxW="600px">
               <Image
-                src="/headline-1.png"
-                m="10"
-                bgRepeat="no-repeat"
-                bgSize="contain"
-                h="60vh"
-                width="1020"
-                height="780"
-                layout="intrinsic"
-                alt="Headline 2"
+                src={"/headline-1.png"}
+                m={"10"}
+                bgRepeat={"no-repeat"}
+                bgSize={"contain"}
+                h={"60vh"}
+                width={"1020"}
+                height={"780"}
+                layout={"intrinsic"}
+                alt={"Headline 2"}
               />
             </chakra.div>
             <chakra.div
